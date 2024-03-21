3663b08e372ef23db0c309497984730152dc11e6
Shahid <mdshahid23456789@gmail.com>
2024-03-21 21:32:12 +0530

    complete projet

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,30 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+          Are you still tracking jobs in Excel or Google Sheets? Track and manage your job applications and interviews all in one place. It’s easy. It’s efficient. It’s powerful.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
